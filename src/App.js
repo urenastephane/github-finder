419662742dd07a2ee0b323688b25dbdddf0a21c4
urenastephane <urenastephane@gmail.com>
2020-07-27 15:20:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Navbar from "./components/layout/Navbar";
+import Alert from "./components/layout/Alert";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import About from "./components/pages/About";
+import User from "./components/users/User";
+import GithubState from "./context/github/GithubState";
+const App = () => {
+  //equivalent to redux store with reducers inside actions through hook states (setState)
+  const [alert, setAlert] = useState(null);
 
-function App() {
+  //equivalent to redux actions
+
+  const showAlert = (msg, type) => {
+    setAlert({ msg, type });
+    setTimeout(() => setAlert(null), 5000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubState>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search setAlert={showAlert} />
+                    <Users />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' component={User} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </GithubState>
   );
-}
+};
 
 export default App;
+
+/*
+  async componentDidMount() {
+    console.log('secret key for github unlimited requests')
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET)
+    this.setState({ loading: true })
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    console.log('res.data')
+    console.log(res.data)
+    this.setState({ users: res.data, loading: false })
+  }
+*/
